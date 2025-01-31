feb5a0aa93dd45261ad747f517c4e1671ce5e6de
treas <treas@Stevu>
2025-01-31 14:19:03 +0530

    Wrong diff program

---
+++ src/main/java/com/obsquara/MavenProjectSample/DifferenceOfNumbers.java
@@ -0,0 +1,12 @@
+package com.obsquara.MavenProjectSample;
+
+public class DifferenceOfNumbers {
+
+	public static void main(String[] args) {
+		int a =30;
+		int b =20;
+		int dif = a-b;
+		System.out.println(dif).
+	}
+
+}
