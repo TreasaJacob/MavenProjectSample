c75da2dc9288a4d8d068c84865a586774b914044
treas <treas@Stevu>
2025-01-31 14:08:26 +0530

    Multiplication of 2 numbers

---
+++ src/main/java/com/obsquara/MavenProjectSample/MultiplyNumbers.java
@@ -0,0 +1,13 @@
+package com.obsquara.MavenProjectSample;
+
+public class MultiplyNumbers {
+
+	public static void main(String[] args) {
+		int a =10;
+		int b =20;
+		int mul = a*b;
+		System.out.println(mul);
+
+	}
+
+}
